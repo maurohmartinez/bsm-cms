9aa2a8affd2a2170e96b128922cb134cc20726e4
mauromartinez <maurohmartinez@gmail.com>
2024-09-04 11:41:16 +0300

    WIP

--- app/Http/Controllers/StudentCrudController.php
+++ app/Http/Controllers/StudentCrudController.php
@@ -103,7 +103,7 @@ class StudentCrudController extends CrudController
         CRUD::field('city')->size(4);
         CRUD::field('birth')->size(4)->type('date_picker');
         CRUD::field('personal_code')->size(6);
-        CRUD::field('personal_code')->size(6);
+        CRUD::field('passport')->size(6);
         CRUD::field('images')->type('upload_multiple')->withFiles(true);
     }
 
