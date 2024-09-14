62ba94a4a1da12e3058d8fd1bce16005f1da493d
mauromartinez <maurohmartinez@gmail.com>
2024-09-14 16:28:09 +0300

    WIP

--- config/backpack/ui.php
+++ config/backpack/ui.php
@@ -73,10 +73,10 @@ return [
     // ------
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'Cristian Tabacitu',
+    'developer_name' => 'Mauro Martinez',
 
     // Developer website. Link in footer. Type false if you want to hide it.
-    'developer_link' => 'http://tabacitu.ro',
+    'developer_link' => 'https://digitalpulse.lv/',
 
     // Show powered by Laravel Backpack in the footer? true/false
     'show_powered_by' => true,
