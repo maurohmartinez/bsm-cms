5e0c7b0b9deb32db2b06860cdd206b01be928961
mauromartinez <maurohmartinez@gmail.com>
2024-09-19 15:22:51 +0300

    WIP

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -14,6 +14,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Str;
@@ -57,7 +58,11 @@ class StudentController extends Controller
         $end = $request->input('end');
         $limit = 1;
 
-        if (Carbon::make($start)->diffInWeeks(Carbon::now()) > $limit || Carbon::make($end)->diffInWeeks(Carbon::now()) > $limit) {
+        if (
+            App::isProduction()
+            && (Carbon::make($start)->diffInWeeks(Carbon::now()) > $limit
+            || Carbon::make($end)->diffInWeeks(Carbon::now()) > $limit)
+        ) {
             return new JsonResponse([]);
         }
 
