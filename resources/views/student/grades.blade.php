5df44c391a689d72747d3b9758a4f9d5c31933e1
mauromartinez <maurohmartinez@gmail.com>
2024-10-31 19:33:27 +0200

    WIP

--- resources/views/student/grades.blade.php
+++ resources/views/student/grades.blade.php
@@ -26,13 +26,16 @@
                                     <td>{{ \Illuminate\Support\Facades\Auth::guard(\App\Models\Student::GUARD)->user()->attendance()
                                         ->whereHas('lesson', function (\Illuminate\Database\Eloquent\Builder $query) use ($subject) {
                                             $query->where('subject_id', $subject->id);
-                                        })->count() }}/{{ $subject->hours }} <span class="text-muted">|</span> {{ round((\Illuminate\Support\Facades\Auth::guard(\App\Models\Student::GUARD)->user()->attendance()
+                                        })->count() }}/{{ $subject->hours }} <small class="text-muted">|</small> {{ round((\Illuminate\Support\Facades\Auth::guard(\App\Models\Student::GUARD)->user()->attendance()
                                         ->whereHas('lesson', function (\Illuminate\Database\Eloquent\Builder $query) use ($subject) {
                                             $query->where('subject_id', $subject->id);
                                         })->count() * 100) / $subject->hours) }}%</td>
-                                    <td>-</td>
-                                    <td>-</td>
-                                    <td>-</td>
+                                    @php
+                                        $grade = \Illuminate\Support\Facades\Auth::guard(\App\Models\Student::GUARD)->user()->grades()->where('subject_id', $subject->id)->first();
+                                    @endphp
+                                    <td>{{ $grade?->participation ?? '-' }}<small class="text-muted">/100</small></td>
+                                    <td>{{ $grade?->exam ?? '-' }}<small class="text-muted">/100</small></td>
+                                    <td>?<small class="text-muted">/100</small></td>
                                 </tr>
                             @endforeach
                             </tbody>
