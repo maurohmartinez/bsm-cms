79be124640d67680ee3568ab12ada1704a8a04e0
mauromartinez <maurohmartinez@gmail.com>
2025-01-30 10:40:23 +0200

    WIP

--- resources/views/student/grades.blade.php
+++ resources/views/student/grades.blade.php
@@ -20,22 +20,21 @@
                             </thead>
                             <tbody>
                             @foreach($subjects as $subject)
+                                @php
+                                    $totalAttendanceCount = \Illuminate\Support\Facades\Auth::guard(\App\Models\Student::GUARD)->user()->attendance()
+                                        ->whereHas('lesson', function (\Illuminate\Database\Eloquent\Builder $query) use ($subject) {
+                                            $query->where('subject_id', $subject->id);
+                                        })->count();
+                                    $grade = \Illuminate\Support\Facades\Auth::guard(\App\Models\Student::GUARD)->user()->grades()->where('subject_id', $subject->id)->first();
+                                    $attendanceGrade = \App\Services\SubjectService::calculateAttendanceGrade($totalAttendanceCount, $subject->hours);
+                                @endphp
                                 <tr>
                                     <td>{{ $subject->name }}</td>
                                     <td>{{ $subject->teacher->name }}</td>
-                                    <td>{{ \Illuminate\Support\Facades\Auth::guard(\App\Models\Student::GUARD)->user()->attendance()
-                                        ->whereHas('lesson', function (\Illuminate\Database\Eloquent\Builder $query) use ($subject) {
-                                            $query->where('subject_id', $subject->id);
-                                        })->count() }}/{{ $subject->hours }} <small class="text-muted">|</small> {{ round((\Illuminate\Support\Facades\Auth::guard(\App\Models\Student::GUARD)->user()->attendance()
-                                        ->whereHas('lesson', function (\Illuminate\Database\Eloquent\Builder $query) use ($subject) {
-                                            $query->where('subject_id', $subject->id);
-                                        })->count() * 100) / $subject->hours) }}%</td>
-                                    @php
-                                        $grade = \Illuminate\Support\Facades\Auth::guard(\App\Models\Student::GUARD)->user()->grades()->where('subject_id', $subject->id)->first();
-                                    @endphp
+                                    <td>{{ $totalAttendanceCount }}/{{ $subject->hours }} <small class="text-muted">|</small> {{ $attendanceGrade }}%</td>
                                     <td>{{ $grade?->participation ?? '-' }}<small class="text-muted">/100</small></td>
                                     <td>{{ $grade?->exam ?? '-' }}<small class="text-muted">/100</small></td>
-                                    <td>?<small class="text-muted">/100</small></td>
+                                    <td>{{ $grade?->exam && $grade?->participation ? \App\Services\SubjectService::calculateFinalGrade($grade->exam, $grade->participation, $attendanceGrade) : '-' }}<small class="text-muted">/100</small></td>
                                 </tr>
                             @endforeach
                             </tbody>
