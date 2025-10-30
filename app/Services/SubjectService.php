d7b27c132bf39242c759e54a4e9c359907a46e33
mauromartinez <maurohmartinez@gmail.com>
2025-10-30 10:38:43 +0200

    WIP

--- app/Services/SubjectService.php
+++ app/Services/SubjectService.php
@@ -12,7 +12,7 @@ class SubjectService
 
     public static function calculateFinalGrade(int $exams, int $participation, int $attendance): int
     {
-        return round((min(100, $exams) * self::EXAMS) / 100)
+        return floor((min(100, $exams) * self::EXAMS) / 100)
             + round((min(100, $participation) * self::PARTICIPATION_NOTES) / 100)
             + round((min(100, $attendance) * self::ATTENDANCE) / 100);
     }
